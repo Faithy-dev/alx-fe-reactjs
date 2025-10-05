58ce4e78edcb588b15dfc1ba65862baa09a62c11
Faithy.dev <fathiaolusegun@gmail.com>
2025-10-06 00:17:15 +0100

    Update TodoList.test.js

--- react-todo/src/_tests_/TodoList.test.js
+++ react-todo/src/_tests_/TodoList.test.js
@@ -2,37 +2,53 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import TodoList from "../components/TodoList";
 
-test("renders initial todos", () => {
-  render(<TodoList />);
-  expect(screen.getByText("Learn React")).toBeInTheDocument();
-  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
-});
+// eslint-disable-next-line no-undef
+describe("TodoList Component", () => {
+  // eslint-disable-next-line no-undef
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    // eslint-disable-next-line no-undef
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    // eslint-disable-next-line no-undef
+    expect(screen.getByText("Build a project")).toBeInTheDocument();
+  });
 
-test("adds a new todo", () => {
-  render(<TodoList />);
-  const input = screen.getByPlaceholderText("Add a new todo");
-  const addButton = screen.getByText("Add");
+  // eslint-disable-next-line no-undef
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByTestId("todo-input");
+    const button = screen.getByText("Add");
 
-  fireEvent.change(input, { target: { value: "New Todo" } });
-  fireEvent.click(addButton);
+    fireEvent.change(input, { target: { value: "New Task" } });
+    fireEvent.click(button);
 
-  expect(screen.getByText("New Todo")).toBeInTheDocument();
-});
+    // eslint-disable-next-line no-undef
+    expect(screen.getByText("New Task")).toBeInTheDocument();
+  });
 
-test("toggles a todo's completion status", () => {
-  render(<TodoList />);
-  const todo = screen.getByText("Learn React");
-  fireEvent.click(todo);
+  // eslint-disable-next-line no-undef
+  test("toggles a todo's completion status", () => {
+    render(<TodoList />);
+    const todoItem = screen.getByText("Learn React");
 
-  expect(todo).toHaveStyle("text-decoration: line-through");
-  fireEvent.click(todo);
-  expect(todo).not.toHaveStyle("text-decoration: line-through");
-});
+    fireEvent.click(todoItem);
+    // eslint-disable-next-line no-undef
+    expect(todoItem).toHaveClass("completed");
 
-test("deletes a todo", () => {
-  render(<TodoList />);
-  const deleteButton = screen.getAllByText("Delete")[0];
-  fireEvent.click(deleteButton);
+    fireEvent.click(todoItem);
+    // eslint-disable-next-line no-undef
+    expect(todoItem).not.toHaveClass("completed");
+  });
 
-  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+  // eslint-disable-next-line no-undef
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const todoItem = screen.getByText("Learn React");
+    const deleteButton = todoItem.nextSibling;
+
+    fireEvent.click(deleteButton);
+
+    // eslint-disable-next-line no-undef
+    expect(todoItem).not.toBeInTheDocument();
+  });
 });
