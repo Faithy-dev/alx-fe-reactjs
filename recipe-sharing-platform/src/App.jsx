be352306753b0daf726dbbb20bf6f3b45cd08677
Faithy.dev <fathiaolusegun@gmail.com>
2025-10-05 20:55:09 +0100

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,18 @@
-
-import HomePage from './components/HomePage'
+// src/App.jsx
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RecipeDetail from "./RecipeDetail"; // make sure this file exists
+import Home from "./Home"; // a homepage component
 
 function App() {
-  
-
   return (
-    <>
-    <HomePage />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
